b0465a22f9460dff0bf779a9dd53d7eee40da52b
Robin Edman <robin.edman@gmail.com>
2016-05-09 22:49:39 +0100

    Test runner can now handle SCSS imports

---
+++ runtime.webpack.config.js
@@ -0,0 +1,17 @@
+// See http://browniefed.com/blog/css-modules-webpack-and-testing/
+var path = require('path');
+
+module.exports = {
+  output: {
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"],
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  },
+};
