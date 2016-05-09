6f0302627426faaae1211946c30727b60288599c
Robin Edman <robin.edman@gmail.com>
2016-05-09 12:35:09 +0100

    SASS support

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import styles from './App.scss';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div className="App">
+        <h1>Hello, world.</h1>
+      </div>
     );
   }
 }
