1eab8733bc22095e2cd3c47e4847b12a750b82b3
Robin Edman <robin.edman@gmail.com>
2016-05-09 22:35:14 +0100

    Added test. Commented out CSS import for now

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import test from 'ava';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+test('has a .App class name', t => {
+    const wrapper = shallow(<App/>);
+    t.true(wrapper.hasClass('App'));
+});
