3863a6a08d52f258c4503263104383c35eee814e
Robin Edman <robin.edman@gmail.com>
2016-05-10 15:53:59 +0100

    Changed tests to match JS style

--- test/App.test.js
+++ test/App.test.js
@@ -1,9 +1,9 @@
-import test from 'ava';
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../src/App';
+import test from 'ava'
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from '../src/App'
 
 test('has a .App class name', t => {
-  const wrapper = shallow(<App/>);
-  t.true(wrapper.hasClass('App'));
-});
+  const wrapper = shallow(<App/>)
+  t.true(wrapper.hasClass('App'))
+})
